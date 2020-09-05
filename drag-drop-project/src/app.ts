e660037bd4ac570504dc8ad01faa9bf468534739
gaylonalfano <gaylon.alfano@gmail.com>
2020-09-05 14:09:15 +0800

    Created an abstract class Component that cannot be instantiated directly, but is used for inheritance purposes. This is a base class that does some general rendering and attaches the component to the DOM. We used Generic types to make this class more generalized so when we do inherit from it, we can specify the type of hostElement to target and the type the element to target/select for this.element. Also used abstract configure(): void and abstract renderContent(): void to force other classes that inherit this Component class to also add these two methods. This way the specific configuration and concrete content can be handled by these other classes that extend from our Component base class. In the end, we have refactored quite a bit. Next, need to actually extend from this base class within ProjectList class and others.

--- drag-drop-project/src/app.ts
+++ drag-drop-project/src/app.ts
@@ -152,6 +152,48 @@ function autobind(
   return adjustedDescriptor;
 }
 
+
+// Component Base Class. Mark abstract class so can't instantiate it directly
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  // Reaching out to our template id="project-list"
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  // Our concrete (section) element we're rendering (just like form in ProjectInput)
+  element: U;
+
+  // Add constructor() that specifies the IDs of the template to select,
+  // the hostElement to render our Component, and for the new rendered element
+  constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string) {
+    this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    // Let's target/select the section as it's the firstElementChild to template
+    this.element = importedNode.firstElementChild as U;
+    // Set a dynamic id= if newElementId? is passed
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+
+    this.attach(insertAtStart);
+  }
+
+  // Insert our importedNode content (this.element) into the document via hostElement
+  // Adding an insertAtStart parameter to specify location
+  private attach(insertAtStart: boolean) {
+    // Check the location argument to insert at either afterbegin or beforeend
+    this.hostElement.insertAdjacentElement(
+      insertAtStart ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+  // Ensure that inheriting classes also add these two methods for more
+  // concrete implementations.
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
+
+
 class ProjectList {
   // Reaching out to our template id="project-list"
   templateElement: HTMLTemplateElement;
