2d208a2de1380a02dc40d4f4ce6d9ef489d2b573
gaylonalfano <gaylon.alfano@gmail.com>
2020-09-07 20:58:51 +0800

    Added a interface Draggable and an interface DragTarget. Each interface has some event handler methods inside. The idea/goal here is to use these interfaces inside any classes that renders an element that can be draggable i.e. ProjectItem. We're also going to make the Active Projects and Finished Projects boxes as valid drop targets as well. The interfaces are not for enforcing a data structure like we've used in the past. Instead, these interfaces force classes that implements Draggable to also create their own class methods of the event handlers we defined inside Draggable and DragTarget. This is similar to how the abstract configure() and renderContent() methods worked inside Component base class as all deriving classes must now create their own concrete/specific version of configure() and renderContent(). So, inside ProjectItem we added the dragStartHandler(event: DragEvent) and the dragEndHandler(_: DragEvent) methods. We also must add the draggable=true attribute inside the HTML element to tell the browser that this element will be draggable. Next, we're going to work on the DragTarget class to make some valid drop targets.

--- drag-drop-project/src/app.ts
+++ drag-drop-project/src/app.ts
@@ -1,3 +1,22 @@
+// Drag and Drop Interfaces
+interface Draggable {
+  // We're going to add listeners to the thing that is draggable
+  // Therefore, we're going to add the handlers here for those event listeners
+  // DragEvent is a built-in TS type
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  // Add event handlers to signal that we're dragging over a valid target
+  dragOverHandler(event: DragEvent): void;
+  // React to the actual drop that happens. Could update UI
+  dropHandler(event: DragEvent): void;
+  // Revert visual update if drag/drop is cancelled
+  dragLeaveHandler(event: DragEvent): void;
+}
+
+
 // Project Type (Class actually so we can instantiate it)
 enum ProjectStatus {
   Active,
@@ -220,7 +239,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 // Create a ProjectItem class that can be instantiated with each new project
 // Responsible for rendering a single project item
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
   // Store the actual project for this instance as a property
   private project: Project;
 
@@ -243,7 +262,26 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
     this.renderContent();
   }
 
-  protected configure() {};
+  // Add our Draggable event handler methods since we implements Draggable
+  // Use @autobind since this handler is used within addEventListener()
+  // so 'this' refers to the class and not the target of the event
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    // Log the event when it occurs
+    console.log(event);
+  }
+
+  /* @autobind */
+  /* dragEndHandler(event: DragEvent) { */
+  dragEndHandler(_: DragEvent) {
+    console.log('DragEnd');
+  }
+
+  protected configure() {
+    // Add event listener to our rendered element for our Draggable event handlers
+    this.element.addEventListener("dragstart", this.dragStartHandler)
+    this.element.addEventListener("dragend", this.dragEndHandler)
+  };
   
   protected renderContent() {
     // Reach out to the elements and insert our project details
